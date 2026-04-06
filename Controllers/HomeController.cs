977facdf89fc967f7166cb92883fe49416e18e3b
Maria Angelina Guerta Ligo <mguertal@my.centennialcollege.ca>
2026-04-06 10:03:40 -0400

    Add database access confirmation message

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,9 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewBag.Message =
+            "Database access has already been done on April 6, 2026 by Maria Angelina Guerta Ligo";
+
         return View();
     }
 
